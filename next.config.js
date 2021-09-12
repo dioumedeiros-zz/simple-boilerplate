f8ea5a2866483c069041b0a648c1fcb7161aba5d
Diou Medeiros <diou.182@gmail.com>
2021-09-11 21:14:52 -0300

    :sparkles: Configured pwa

--- next.config.js
+++ next.config.js
@@ -1,3 +1,11 @@
-module.exports = {
-  reactStrictMode: false
-}
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withPWA = require('next-pwa')
+const isProd = process.env.NODE_ENV === 'production'
+
+module.exports = withPWA({
+  reactStrictMode: false,
+  pwa: {
+    dest: 'public',
+    disable: !isProd
+  }
+})
--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,7 +1,18 @@
+import * as S from './styles'
+
 const Main = () => (
-  <main>
-    <h1>React Avançado</h1>
-  </main>
+  <S.Wrapper>
+    <S.Logo
+      src="/img/logo.svg"
+      alt="Imagem de um átomo  e React Avaçado escrito ao lado."
+    />
+    <S.Title>React Avançado</S.Title>
+    <S.Description>Typescritp, ReactJS, NextJS</S.Description>
+    <S.Illustration
+      src="/img/hero-illustration.svg"
+      alt="Um desenvolvedor de frente para o computador"
+    />
+  </S.Wrapper>
 )
 
 export default Main
